0b2ebee0ddda57e40323dc1b15303d852a8f770b
ketan <ketan13raul@gmail.com>
2021-12-22 19:56:02 -0800

    Initial commit

---
+++ WebApplication2/Models/EmployeeDtl.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApplication2.Models
+{
+    public partial class EmployeeDtl
+    {
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Designation { get; set; }
+        public decimal Salary { get; set; }
+        public DateTime JoiningDate { get; set; }
+    }
+}
---
+++ WebApplication2/Models/EmployeeDtlsContext.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace WebApplication2.Models
+{
+    public partial class EmployeeDtlsContext : DbContext
+    {
+        public EmployeeDtlsContext()
+        {
+        }
+
+        public EmployeeDtlsContext(DbContextOptions<EmployeeDtlsContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<EmployeeDtl> EmployeeDtl { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning       optionsBuilder.UseSqlServer("Server=(local);Database=EmployeeDtls;Trusted_Connection=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<EmployeeDtl>(entity =>
+            {
+                entity.HasKey(e => e.EmployeeId);
+
+                entity.Property(e => e.EmployeeId).HasColumnName("EmployeeID");
+
+                entity.Property(e => e.Address)
+                    .HasMaxLength(250)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Designation)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.JoiningDate).HasColumnType("datetime");
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Salary).HasColumnType("decimal(18, 0)");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
